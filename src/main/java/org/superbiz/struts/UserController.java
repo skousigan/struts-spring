a1466e167222a052c9364038c6aee410d7a6ba97
Kousigan Srinivasan <kousigan.srinivasan@cognizant.com>
2020-04-18 17:18:51 +0530

    StrutsToSpring

---
+++ src/main/java/org/superbiz/struts/UserController.java
@@ -0,0 +1,61 @@
+package org.superbiz.struts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+
+@Controller
+public class UserController {
+    private final UserRepository userRepository;
+    User user;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestParam long id, @RequestParam String firstName, @RequestParam String lastName, Model model ) {
+        try {
+            userRepository.save(new User(id, firstName, lastName));
+        } catch (Exception e) {
+            model.addAttribute("errorMessage",e.getMessage());
+            return "addUserForm";
+        }
+        return "addedUser";
+    }
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isPresent()) {
+            model.addAttribute("user", user.get());
+        } else {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "displayUsers";
+    }
+}
